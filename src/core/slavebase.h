7a2d08780170e6fa0dedd5eb863cfa2ed8dca1ff
Nicolas Fella <nicolas.fella@gmx.de>
2025-09-11 11:35:47 +0200

    Drop HostInfo API from Worker
    This isn't used

--- src/core/slavebase.h
+++ src/core/slavebase.h
@@ -906,16 +906,6 @@ public:
      */
     void setKillFlag();
 
-    /* Internally used
-     * \internal
-     */
-    void lookupHost(const QString &host);
-
-    /* Internally used
-     * \internal
-     */
-    int waitForHostInfo(QHostInfo &info);
-
     /*
      * Checks with job if privilege operation is allowed.
      * Returns privilege operation status.
